76ff2d9540c5076e737130095540f7faa164cf66
impl-EitaHayashizaki <hayashizaki.ei@impl.co.jp>
2024-05-17 16:50:39 +0900

    リポジトリ追加

---
+++ MemoApp/Swift/Contents/Splash/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  MemoApp
+//
+//  Created by Eita Hayashizaki on 2024/05/17.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
